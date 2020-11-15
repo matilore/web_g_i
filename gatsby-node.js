5c5752483a697e66be7a87f215ffafeb2c603fb5
Matias Lorenzo <matiaslorenzo@Matiass-MacBook-Pro.local>
2020-11-15 13:58:21 +0100

    convert to es6

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,31 +4,31 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const { languages, getLocalizedPath } = require("./src/i18n");
+import { languages, getLocalizedPath } from './src/i18n'
 
-exports.onCreatePage = ({ page, actions }) => {
-    const { createPage, deletePage } = actions;
+export function onCreatePage({ page, actions }) {
+    const { createPage, deletePage } = actions
 
-    if (page.internalComponentName === "ComponentDev404Page") {
-        return;
+    if (page.internalComponentName === 'ComponentDev404Page') {
+        return
     }
 
-    return new Promise(resolve => {
-        deletePage(page);
+    return new Promise((resolve) => {
+        deletePage(page)
 
-        languages.forEach(lang => {
-            const localizedPath = getLocalizedPath(page.path, lang.locale);
+        languages.forEach((lang) => {
+            const localizedPath = getLocalizedPath(page.path, lang.locale)
             const localePage = {
                 ...page,
                 path: localizedPath,
                 context: {
                     locale: lang.locale,
-                    originalPath: page.path
-                }
-            };
-            createPage(localePage);
-        });
+                    originalPath: page.path,
+                },
+            }
+            createPage(localePage)
+        })
 
-        resolve();
-    });
-};
+        resolve()
+    })
+}
