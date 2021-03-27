2fd2be429671e5d2a5673ee65cc7a1fcff745490
Matias Lorenzo <matiaslorenzo@Matiass-MacBook-Pro.local>
2021-03-27 20:03:13 +0100

    change map

--- src/components/contact/map.jsx
+++ src/components/contact/map.jsx
@@ -9,7 +9,7 @@ const StyledMap = styled.img`
 const Map = ({ width = 500, height = 250 }) =>
     typeof window !== 'undefined' ? (
         <StyledMap
-            src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/10.89,44.6437,15,0/${width}x${height}?access_token=sk.eyJ1IjoibWF0aWxvcmUiLCJhIjoiY2todGhzMXg1MW5hbDMwcDVneDA5Mm05eCJ9.CEs6HiZn9R5qkMK6-YKxZQ`}
+            src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+841515(10.8901,44.6437)/10.9174,44.6395,11,0/${width}x${height}?access_token=pk.eyJ1IjoibWF0aWxvcmUiLCJhIjoiY2todGhwbW00MTJxejJycGJya2pvMnRqcCJ9.477I2zziqQm1EMjHJyvs1g`}
         />
     ) : null
 
